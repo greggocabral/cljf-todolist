36292676c60ce119cf16a22a48b0b03876b4fcc4
Gregorio Cabral <cabral.gr@gmail.com>
2019-07-26 09:16:58 -0300

    Initial commit

---
+++ src/webdev/core.clj
@@ -0,0 +1,24 @@
+(ns webdev.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
+
+(defn greet [req]
+ {:status 200 :body "Hello, Grego!" :headers {}})
+
+(defn goodbye [req]
+ {:status 200 :body "Goodbye, cruel world" :headers {}})
+
+(defroutes app
+  (GET "/" [] greet)
+  (GET "/goodbye" [] goodbye)
+  (not-found "Page not found"))
+
+(defn -main [port]
+  (jetty/run-jetty app
+                   {:port (Integer. port)}))
+
+(defn -dev-main [port]
+  (jetty/run-jetty (wrap-reload (var app))
+                   {:port (Integer. port)}))
